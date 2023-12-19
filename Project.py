e206f8eb8091f19e0e1692afe8cbb4b2efaebec5
Kevin Rasmussen <kevinrasmussen@Kevins-MacBook-Pro.local>
2023-12-19 12:38:37 -0600

    Corrected game logic

--- Project.py
+++ Project.py
@@ -19,7 +19,7 @@ planet_list = ("Naboo", "Bespin", "Iium", "Umbara")
 planet1 = Worlds("Naboo", "Swampland", "Fishing")
 planet2 = Worlds("Bespin", "Gas Giant", "Fuel")
 planet3 = Worlds("Iium", "Rocky", "Crystals")
-planet4 = Worlds("Umbara", "Shadow World", "Doonium")
+planet4 = Worlds("Umbara", "Shadow World", "Doonium Ore")
 
 
 class Explorers:
@@ -36,12 +36,14 @@ class Explorers:
         self.doonium_inventory = []
 
     def __repr__(self):
-        return "the explorer {name} who specializes in {skills} and has an inventory of {fish} Fish, {fuel_cells} Fuel Cells, {crystals} Crystals, and {doonium} Doonium Ores.".format(
+        return "the explorer {name} who specializes in {skills} and has an inventory of {fish} Fish, {fuel_cells} Fuel Cells, {crystals} Crystals, and {doonium} Doonium Ore.".format(
             name=self.name, skills=self.skills, fish=self.fish, fuel_cells=self.fuel_cells, crystals=self.crystals,
             doonium=self.doonium)
 
     def select_activity(self):
-        actions = ["Mine Fuel Cells", "Fishing", "Mine Doonium", "Mine Crystals"]
+        actions = ["Mine Fuel Cells", "Fishing", "Mine Doonium Ore", "Mine Crystals"]
+        print(""
+              "")
         print("What activity would you like to do?")
         for i, action in enumerate(actions, 1):
             print(f"{i}. {action}")
@@ -59,47 +61,68 @@ class Explorers:
             print("Invalid activity.")
 
     def travel(self):
-        while True:
-            travel_worlds = input(
-                "Where would you like to travel to today? Your current choices are " + str(planet_list) + " ")
-            if str(travel_worlds) == "Naboo":
-                print(travel_worlds + " is a great destination! " + str(planet1))
-            elif str(travel_worlds) == "Bespin":
-                print(travel_worlds + " is a great destination! " + str(planet2))
-            elif str(travel_worlds) == "Iium":
-                print(travel_worlds + " is a great destination! " + str(planet3))
-            elif str(travel_worlds) == "Umbara":
-                print(travel_worlds + " is a great destination! " + str(planet4))
-            else:
-                print("Sorry this destination is not available!")
+        world_list = [planet1, planet2, planet3, planet4]
+        chosen_planet = None
 
-            cont = input("Would you like to enter another destination? (yes/no)")
-            if cont.lower() != 'yes':
+        while chosen_planet is None:
+            print("\nWhere would you like to travel to today? Your current choices are:")
+            for i, world in enumerate(world_list, start=1):
+                print(str(i) + ". " + world.name)
+
+            try:
+                travel_worlds = int(input())
+                chosen_planet = world_list[travel_worlds - 1]
+                print(chosen_planet.name + " is a great destination! ")
+                print(str(chosen_planet))
+                print("\n")
+            except (IndexError, ValueError):
+                print("Sorry, this destination is not available!")
+
+            cont = input("Would you like to enter another destination? (yes/no) ")
+            if not cont.lower() in ['yes', 'y']:
                 break
+            else:
+                chosen_planet = None # reset to None so user can choose again
 
     def mine_crystals(self):
         mined_crystals = random.randint(0, 20)  # Random amount of crystals mined
         self.crystals += mined_crystals
         self.crystal_inventory.append(mined_crystals)
-        print(f"{self.name} mined {mined_crystals} crystals!")
+        print(""
+              "")
+        print(f"******{self.name} mined {mined_crystals} crystals!******")
+        print(""
+              "")
 
     def mine_fish(self):
         mined_fish = random.randint(0, 20)  # Random amount of fish mined
         self.fish += mined_fish
         self.fish_inventory.append(mined_fish)
-        print(f"{self.name} mined {mined_fish} fish!")
+        print(""
+              "")
+        print(f"******{self.name} mined {mined_fish} fish!******")
+        print(""
+              "")
 
     def mine_doonium(self):
         mined_doonium = random.randint(0, 20)  # Random amount of Doonium mined
         self.doonium += mined_doonium
         self.doonium_inventory.append(mined_doonium)
-        print(f"{self.name} mined {mined_doonium} Doonium!")
+        print(""
+              "")
+        print(f"******{self.name} mined {mined_doonium} Doonium Ore!******")
+        print(""
+              "")
 
     def mine_fuel_cells(self):
         mined_fuel_cells = random.randint(0, 20)  # Random amount of fuel cells mined
         self.fuel_cells += mined_fuel_cells
         self.fuel_inventory.append(mined_fuel_cells)
-        print(f"{self.name} mined {mined_fuel_cells} fuel cells!")
+        print(""
+              "")
+        print(f"******{self.name} mined {mined_fuel_cells} fuel cells!******")
+        print(""
+              "")
 
 
 character1 = Explorers("Aesira", "Fishing")
@@ -120,6 +143,8 @@ print(
  '''
 )
 print("Welcome to Star Adventures!")
+print(""
+      "")
 print("Choose a character:")
 for index, character in enumerate(characters):
     print(f"{index + 1}. {character.name}")
@@ -130,6 +155,8 @@ choice = int(input("Enter the number of the character you want to choose: ")) - 
 
 if choice in range(len(characters)):
     selected_character = characters[choice]
+    print(""
+          "")
     print(f"You have selected {selected_character}")
 
     selected_character.travel()
@@ -142,21 +169,26 @@ if choice in range(len(characters)):
         # Inside main game loop:
 
         cont = input("Would you like to perform another activity? (yes/no)")
-        if cont.lower() == 'yes':
-            # continue with your code
+        if cont.lower() == 'yes' or cont.lower() == 'y':
             pass
-        elif cont.lower() == 'no':
+        elif cont.lower() == 'no' or cont.lower() == 'n':
             while True:
-                print("What would you like to do?")
+                print("\nWhat would you like to do?")
+                print(""
+                      "")
                 print("1. Quit Game")
                 print("2. Travel to a Different Planet")
                 print("3. Select Another Activity")
                 next_action = input("Enter the number of your choice: ")
 
                 if next_action == "1":
+                    print(""
+                          "")
                     print(f"{selected_character.name} ended the game with the following inventory:")
+                    print(""
+                          "")
                     print(
-                        f"Fish: {selected_character.fish}, Fuel cells: {selected_character.fuel_cells}, Crystals: {selected_character.crystals}, Doonium: {selected_character.doonium}")
+                        f"******Fish: {selected_character.fish}, Fuel cells: {selected_character.fuel_cells}, Crystals: {selected_character.crystals}, Doonium Ore: {selected_character.doonium}******")
                     game_is_running = False  # stop the game
                     break
                 elif next_action == "2":
